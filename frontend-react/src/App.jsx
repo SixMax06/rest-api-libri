4bb462b532c12167003a65b9dea247dffe6996f5
SixMax06 <alemilamax@gmail.com>
2026-01-16 12:46:00 +0100

    inizio frontend
    GET al backend funziona, iniziata l'implementazione del form
    
    TODO: ricerca, POST al backend, visualizzazione dei libri, DELETE al backend

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const rest_api_url = "http://localhost:11000/api/libri"
+let data = await fetch(rest_api_url)
+data = await data.json()
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [titolo, setTitolo] = useState('')
+  const [autore, setAutore] = useState('')
+  const [genere, setGenere] = useState('')
+  const [annoPubblicazione, setAnnoPubblicazione] = useState('')
+
+  console.log(data)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*Form di input*/}
+      <form>
+        <input type="text" placeholder='Titolo' value={titolo} onChange={(event) => setTitolo(event.target.value)}/>
+        <input type="text" placeholder='Autore' value={autore} onChange={(event) => setAutore(event.target.value)}/>
+        <input type="text" placeholder='Genere' value={genere} onChange={(event) => setGenere(event.target.value)}/>
+        <input type="text" placeholder='Anno di pubblicazione' value={annoPubblicazione} onChange={(event) => setAnnoPubblicazione(event.target.value)}/>
+        <button type="submit">Cerca</button>
+        <button type="submit">Aggiungi</button>
+      </form>
+      <div></div>
     </>
   )
 }
